6bdb53f16d76006fd5726a5fce09cb5996ae9cb2
hikasuun <77881556+hikasuun@users.noreply.github.com>
2023-02-10 03:47:19 -0600

    Further Development to Scrapper

--- StreamBox/Python/ScheduleScrapper.py
+++ StreamBox/Python/ScheduleScrapper.py
@@ -1,25 +1,39 @@
 from bs4 import BeautifulSoup
 import requests
 import re
-from deep_translator import GoogleTranslator
-import sys
+from datetime import datetime, timedelta
+import pytz
 
-urlList = ['https://schedule.hololive.tv/lives/hololive',
-           'https://schedule.hololive.tv/lives/english',
-           'https://schedule.hololive.tv/lives/indonesia']
+urlList = ['https://schedule.hololive.tv/lives/english']
+currentDateInTokyo = datetime.now().date()
+previousDateInTokyo = (currentDateInTokyo - timedelta(days=1))
+nextDateInTokyo = (currentDateInTokyo + timedelta(days=1))
 
-with open('data', 'w',encoding="utf-8") as file:
-    for x in urlList:
-    # employs BeautifulSoup to scrape data
-        page = requests.get(x)
-        soup = BeautifulSoup(page.content, "html.parser")
-        date = soup.find_all('div', class_='holodule navbar-text')
-        stream_cards = soup.find_all('a',href=re.compile('https://www\.youtube\.com/'))
-        for stream in stream_cards:
-            file.write(stream.find('div', class_='col-4 col-sm-4 col-md-4 text-left datetime').text.replace(' ','').replace('\n', '') + 
-            stream.find('div', class_='col text-right name').text.replace(' ','')  + 
-            stream['href'])
+with open('data', 'w', encoding='utf-8') as file:
+    for url in urlList:
+        # make url req. and scrape
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        for card in soup.find_all('div', 'col-6 col-sm-4 col-md-3') :
+            file.write(card.find('div', class_='col-4 col-sm-4 col-md-4 text-left datetime').text.replace(' ','').replace('\n', '') + 
+                       card.find('div', class_='col text-right name').text.replace(' ','')  + 
+                       card.find('a', href=re.compile('https://www\.youtube\.com/'))['href'] )
             file.write('\n')
 
-#TODO: For each x, keep a counter for when the time goes over 24 hr then increment to next day and post before each stream,
-#      the first date of streams will always be what the is at top of page that is extracted
+                    
+
+''' PREVIOUS WORK FOR TRYING TO GET DATE EXTRACTED AND PLACED IN APPROPRIATE AREA
+        containers = soup.find_all('div', class_='container')
+        for container in containers:
+            if ( (container.find('div',style='padding-left:5px;padding-right: 5px;' )) is None ):
+                pass
+            else:
+                if (container.find('br') is not None):
+                    pass
+                for card in container.find_all('div', 'col-6 col-sm-4 col-md-3'):
+                    file.write(container.find('div', class_='holodule navbar-text').text.replace(' ','').replace('\n','')[0:6] + '\n')
+                    file.write(card.find('div', class_='col-4 col-sm-4 col-md-4 text-left datetime').text.replace(' ','').replace('\n', '') + 
+                               card.find('div', class_='col text-right name').text.replace(' ','')  + 
+                               card.find('a', href=re.compile('https://www\.youtube\.com/'))['href'] )
+                    file.write('\n')
+'''
